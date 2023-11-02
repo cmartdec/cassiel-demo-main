14e55850bb0fa6fda2b9236246a1ce9b92e6d370
Carlos Martinez <91010784+cmartdec@users.noreply.github.com>
2023-11-02 11:52:28 +0100

    credits screen

--- cassiel-demo/ui/menus/title_screen/title_screen.gd
+++ cassiel-demo/ui/menus/title_screen/title_screen.gd
@@ -6,6 +6,9 @@ onready var attenuate_bg = $AtenuateBackground
 func _ready():
 	_menus.connect("options_button_pressed", self, "on_options_button_pressed")
 	_menus.connect("back_button_pressed", self, "on_back_button_pressed")
+	_menus.connect("credits_button_pressed", self, "on_credits_button_pressed")
+	_menus.connect("credits_back_button_pressed", self, "on_credits_back_button_pressed")
+	
 	
 	
 func on_options_button_pressed():
@@ -14,3 +17,8 @@ func on_options_button_pressed():
 func on_back_button_pressed():
 	attenuate_bg.hide()
 	
+func on_credits_button_pressed():
+	attenuate_bg.show()
+	
+func on_credits_back_button_pressed():
+	attenuate_bg.hide()
--- cassiel-demo/ui/menus/title_screen/title_screen_menus.gd
+++ cassiel-demo/ui/menus/title_screen/title_screen_menus.gd
@@ -2,10 +2,12 @@ extends Control
 
 signal options_button_pressed
 signal back_button_pressed
+signal credits_button_pressed
+signal credits_back_button_pressed
 
 onready var main_menu = $MainMenu
 onready var options_menu = $MenuOptions
-
+onready var credits_menu = $MenuCredits
 
 func _on_OptionsButton_pressed():
 	main_menu.hide()
@@ -22,3 +24,15 @@ func _on_Button_pressed():
 func _on_QuitButton_pressed():
 	ProgressData.save()
 	get_tree().quit()
+
+
+func _on_CreditsButton_pressed():
+	main_menu.hide()
+	credits_menu.show()
+	emit_signal("credits_button_pressed")
+
+
+func _on_Back_Button_of_Credits_pressed():
+	main_menu.show()
+	credits_menu.hide()
+	emit_signal("credits_back_button_pressed")
